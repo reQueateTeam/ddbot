33bfff949afde74e9548fbe9dda173a91b926f80
ssysm <me@theeditorstudio.com>
2017-11-29 15:28:58 -0600

    add database setting

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Import Module
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const mongojs = require('mongojs');
-
-
+//Config Working Env
+const db = mongojs('localhost:27017/ddbot');
 const app = express();
-
+//Config Database
+const userCollection = db.collection('user');
+const idolCollection = db.collection('idol');
+//Config Express Setting
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
